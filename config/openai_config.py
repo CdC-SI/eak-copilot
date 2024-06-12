3f5db28c47beffc9d64508b9977ebbfb9c4d0a86
VNM <matthieu.vinciarelli@zas.admin.ch>
2024-06-12 11:14:12 +0200

    adding proxy to the openai client if we find a corresponding environment variable

--- config/openai_config.py
+++ config/openai_config.py
@@ -1,4 +1,5 @@
 import os
+import openai
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
@@ -6,7 +7,16 @@ load_dotenv()
 
 # Load OpenAI API key
 OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+HTTP_PROXY = os.environ.get("HTTP_PROXY", "noproxy")
 
-# Set OpenAI API key
-import openai
-openai.api_key = OPENAI_API_KEY
+# if HTTP_PROXY not noproxy then set the proxy in openai client
+if HTTP_PROXY != "noproxy":
+    import httpx
+    clientAI = openai.OpenAI(
+        http_client=httpx.Client(proxy=HTTP_PROXY),
+        api_key=OPENAI_API_KEY
+    )
+else:
+    clientAI = openai.OpenAI(
+        api_key=OPENAI_API_KEY
+    )
--- utils/embedding.py
+++ utils/embedding.py
@@ -2,13 +2,14 @@ from typing import List, Union
 
 # Import env vars
 from config.base_config import rag_config
-from config.openai_config import openai
+from config.openai_config import clientAI
+
 
 # Function to get embeddings for a text
 def get_embedding(text: Union[List[str], str]):
     model = rag_config["embedding"]["model"]
     if model == "text-embedding-ada-002":
-        response = openai.embeddings.create(
+        response = clientAI.embeddings.create(
             input=text,
             model=model,
         )
